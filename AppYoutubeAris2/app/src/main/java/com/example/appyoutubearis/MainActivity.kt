ebf044680f50e910dce8b21dabc1253d4c9895db
Javier <javdevia@gmail.com>
2023-06-15 12:57:32 -0400

    Funcionando app que saluda

--- AppYoutubeAris2/app/src/main/java/com/example/appyoutubearis/MainActivity.kt
+++ AppYoutubeAris2/app/src/main/java/com/example/appyoutubearis/MainActivity.kt
@@ -1,11 +1,29 @@
 package com.example.appyoutubearis
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.AppCompatButton
+import androidx.appcompat.widget.AppCompatEditText
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //Inicializacion de graficos
+        val btnAcceptName = findViewById<AppCompatButton>(R.id.btnAcceptName)
+        val etName = findViewById<AppCompatEditText>(R.id.etName)
+
+        //Observadores
+        btnAcceptName.setOnClickListener {
+            val name: String = etName.text.toString()
+            if (name.isNotEmpty()) {
+                val intent = Intent(this, ResultActivity::class.java)
+                intent.putExtra("EXTRA_NAME", name)
+                startActivity(intent)
+            }
+        }
+
     }
 }
---
+++ AppYoutubeAris2/app/src/main/java/com/example/appyoutubearis/ResultActivity.kt
@@ -0,0 +1,15 @@
+package com.example.appyoutubearis
+
+import android.os.Bundle
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
+class ResultActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_result)
+        val tvResult = findViewById<TextView>(R.id.saludo)
+        val name: String = intent.extras?.getString("EXTRA_NAME").orEmpty()
+        tvResult.text = "Hola $name"
+    }
+}
