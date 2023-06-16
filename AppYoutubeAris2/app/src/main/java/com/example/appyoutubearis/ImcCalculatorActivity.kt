2bf8f691379725b1ca13f482e5d00d0da2f1c39b
Javier <javdevia@gmail.com>
2023-06-16 16:03:11 -0400

    Se agrega menu funcionando

---
+++ AppYoutubeAris2/app/src/main/java/com/example/appyoutubearis/ImcCalculatorActivity.kt
@@ -0,0 +1,11 @@
+package com.example.appyoutubearis
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ImcCalculatorActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_imc_calculator)
+    }
+}
