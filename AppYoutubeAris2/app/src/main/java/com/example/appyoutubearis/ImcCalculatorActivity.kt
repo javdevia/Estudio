4f26f5dcf472c34fdd0979ed78372ece87a4aba0
Javier <javdevia@gmail.com>
2023-06-17 17:59:09 -0400

    Se agrega botones masculino/femenino a APP IMC

--- AppYoutubeAris2/app/src/main/java/com/example/appyoutubearis/ImcCalculatorActivity.kt
+++ AppYoutubeAris2/app/src/main/java/com/example/appyoutubearis/ImcCalculatorActivity.kt
@@ -1,11 +1,13 @@
 package com.example.appyoutubearis
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
 class ImcCalculatorActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_imc_calculator)
+
+
     }
 }
--- AppYoutubeAris2/app/src/main/java/com/example/appyoutubearis/MenuActivity.kt
+++ AppYoutubeAris2/app/src/main/java/com/example/appyoutubearis/MenuActivity.kt
@@ -1,11 +1,29 @@
 package com.example.appyoutubearis
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
 
 class MenuActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_menu)
+
+        // Inicialización de botones
+        val btnSaludapp = findViewById<Button>(R.id.btnSaludapp)
+        val btnImcCalculator = findViewById<Button>(R.id.btnImcCalculator)
+
+        // Observadores de botones
+        btnSaludapp.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+
+        btnImcCalculator.setOnClickListener {
+            val intent = Intent(this, ImcCalculatorActivity::class.java)
+            startActivity(intent)
+        }
+
     }
 }
