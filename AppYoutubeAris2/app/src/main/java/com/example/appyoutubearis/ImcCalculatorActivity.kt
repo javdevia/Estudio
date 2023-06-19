1ac1a03cba6041a15a5020bb3a2571c37d446206
Javier <javdevia@gmail.com>
2023-06-19 15:43:13 -0400

    Se agrego elementos graficos y se abre aplicacion con logica incompleta

--- AppYoutubeAris2/app/src/main/java/com/example/appyoutubearis/ImcCalculatorActivity.kt
+++ AppYoutubeAris2/app/src/main/java/com/example/appyoutubearis/ImcCalculatorActivity.kt
@@ -2,12 +2,59 @@ package com.example.appyoutubearis
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.cardview.widget.CardView
+import androidx.core.content.ContextCompat
 
 class ImcCalculatorActivity : AppCompatActivity() {
+
+    private var isMaleSelected: Boolean = true
+    private var isFemaleSelected: Boolean = false
+    private lateinit var cvMale: CardView
+    private lateinit var cvFemale: CardView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_imc_calculator)
+        initComponents()
+        initListeners()
+        initUI()
+    }
 
+    private fun initComponents() {
+        cvMale = findViewById(R.id.cvMale)
+        cvFemale = findViewById(R.id.cvFemale)
+    }
 
+    private fun initListeners() {
+        cvMale.setOnClickListener {
+            isMaleSelected = true
+            isFemaleSelected = false
+            setGenderColor()
+        }
+        cvFemale.setOnClickListener {
+            isMaleSelected = false
+            isFemaleSelected = true
+            setGenderColor()
+        }
+    }
+
+    private fun setGenderColor() {
+        cvMale.setCardBackgroundColor(getBackgroundColor(isMaleSelected))
+        cvFemale.setCardBackgroundColor(getBackgroundColor(isFemaleSelected))
+    }
+
+    private fun getBackgroundColor(isSelectedComponent: Boolean): Int {
+
+        val colorReference = if (isSelectedComponent) {
+            R.color.background_component_selected
+        } else {
+            R.color.background_component
+        }
+
+        return ContextCompat.getColor(this, colorReference)
+    }
+
+    private fun initUI() {
+        setGenderColor()
     }
 }
