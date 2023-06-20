3a80d9d9e370e0144a67b643c6537e03f2c7b62c
Javier <javdevia@gmail.com>
2023-06-20 13:02:22 -0400

    Se agrega funcionalidad a botones Add y Subtract de peso y edad

--- AppYoutubeAris2/app/src/main/java/com/example/appyoutubearis/ImcCalculatorActivity.kt
+++ AppYoutubeAris2/app/src/main/java/com/example/appyoutubearis/ImcCalculatorActivity.kt
@@ -6,6 +6,7 @@ import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.cardview.widget.CardView
 import androidx.core.content.ContextCompat
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.slider.RangeSlider
 import java.text.DecimalFormat
 
@@ -13,10 +14,24 @@ class ImcCalculatorActivity : AppCompatActivity() {
 
     private var isMaleSelected: Boolean = true
     private var isFemaleSelected: Boolean = false
+    private var weight: Int = 60
+    private var age: Int = 25
+
     private lateinit var cvMale: CardView
+
     private lateinit var cvFemale: CardView
+
     private lateinit var rsHeight: RangeSlider
     private lateinit var tvHeight: TextView
+
+    private lateinit var fabWeightSubtract: FloatingActionButton
+    private lateinit var fabWeightAdd: FloatingActionButton
+    private lateinit var tvWeight: TextView
+
+    private lateinit var fabAgeSubtract: FloatingActionButton
+    private lateinit var fabAgeAdd: FloatingActionButton
+    private lateinit var tvAge: TextView
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -29,26 +44,57 @@ class ImcCalculatorActivity : AppCompatActivity() {
     private fun initComponents() {
         cvMale = findViewById(R.id.cvMale)
         cvFemale = findViewById(R.id.cvFemale)
-        rsHeight = findViewById(R.id.rsHight)
         tvHeight = findViewById(R.id.tvHeight)
+        rsHeight = findViewById(R.id.rsHight)
+
+        fabWeightSubtract = findViewById(R.id.fabWeightSubtract)
+        fabWeightAdd = findViewById(R.id.fabWeightAdd)
+        tvWeight = findViewById(R.id.tvWeight)
+
+        fabAgeSubtract = findViewById(R.id.fabAgeSubtract)
+        fabAgeAdd = findViewById(R.id.fabAgeAdd)
+        tvAge = findViewById(R.id.tvAge)
     }
 
     @SuppressLint("SetTextI18n")
     private fun initListeners() {
+
         cvMale.setOnClickListener {
             isMaleSelected = true
             isFemaleSelected = false
             setGenderColor()
         }
+
         cvFemale.setOnClickListener {
             isMaleSelected = false
             isFemaleSelected = true
             setGenderColor()
         }
+
         rsHeight.addOnChangeListener { _, value, _ ->
             val df = DecimalFormat("#.##")
             val result = df.format(value)
             tvHeight.text = "$result cm"
+        }
+
+        fabWeightSubtract.setOnClickListener {
+            weight -= 1
+            tvWeight.text = "$weight kg"
+        }
+
+        fabWeightAdd.setOnClickListener {
+            weight += 1
+            tvWeight.text = "$weight kg"
+        }
+
+         fabAgeSubtract.setOnClickListener {
+            age -= 1
+            tvAge.text = age.toString()
+        }
+
+        fabAgeAdd.setOnClickListener {
+            age += 1
+            tvAge.text = age.toString()
         }
 
     }
