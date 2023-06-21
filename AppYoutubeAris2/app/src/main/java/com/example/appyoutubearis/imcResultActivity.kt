2e74fceebde53b31e2aa4d8234eefb5d0818b21b
Javier <javdevia@gmail.com>
2023-06-20 23:47:26 -0400

    Se agrega despliegue de resultados y comentarios

--- AppYoutubeAris2/app/src/main/java/com/example/appyoutubearis/imcResultActivity.kt
+++ AppYoutubeAris2/app/src/main/java/com/example/appyoutubearis/imcResultActivity.kt
@@ -1,33 +1,80 @@
 package com.example.appyoutubearis
 
 import android.os.Bundle
+import android.widget.Button
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.example.appyoutubearis.ImcCalculatorActivity.Companion.IMC_KEY
+import java.text.DecimalFormat
 
 class imcResultActivity : AppCompatActivity() {
 
+
+    private lateinit var tvResultEvaluation: TextView
     private lateinit var tvResultImc: TextView
+    private lateinit var tvResultDetail: TextView
+    private lateinit var btnReCalculate: Button
+    private var IMC = 20.00
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_imc_result)
-
+        IMC = roundIMC()
         initComponents()
+        initListeners()
         initUI()
     }
 
     private fun initComponents() {
+        tvResultEvaluation = findViewById(R.id.tvResultEvaluation)
         tvResultImc = findViewById(R.id.tvResultImc)
+        tvResultDetail = findViewById(R.id.tvResultDetail)
+        btnReCalculate = findViewById(R.id.btnReCalculate)
+    }
+
+    private fun initListeners() {
+        btnReCalculate.setOnClickListener() {
+        }
     }
 
     private fun initUI() {
+        tvResultEvaluation.text = getEvaluation(IMC)
         tvResultImc.text = getImc()
+        tvResultDetail.text = getDetail(IMC)
+    }
+
+    private fun getDetail(IMC: Double): String {
+        val evaluation: String = when (IMC) {
+            in 0.00..18.49 -> "Tu índice de masa corporal está un poco bajo"
+            in 18.50..24.99 -> "Tu índice de masa corporal está Normal"
+            in 25.00..29.99 -> "Tu índice de masa corporal está un poco Alto"
+            in 30.00..100.00 -> "Tu índice demasa corporal está demasiado algo"
+            else -> "Error"
+        }
+        return evaluation
+    }
+
+    private fun getEvaluation(IMC: Double): String {
+        val evaluation: String = when (IMC) {
+            in 0.00..18.49 -> "Bajo"
+            in 18.50..24.99 -> "Normal"
+            in 25.00..29.99 -> "Alto"
+            in 30.00..100.00 -> "Obesidad"
+            else -> "Error"
+        }
+        return evaluation
     }
 
     private fun getImc(): String {
         val imcFinal = intent.extras?.getString(IMC_KEY).orEmpty()
         return imcFinal
+    }
+
+    private fun roundIMC():Double{
+        val roundImc = getImc().toDoubleOrNull()?:0.0
+        val df = DecimalFormat("#.##")
+        val roundedImcString = df.format(roundImc)
+        return roundedImcString.toDouble()
     }
 
 }
